c0bd9fbb95db38539a8ad3ec3e6d5e424047d45a
Paul Riverain <paulriverain@yahoo.com>
2019-01-04 18:59:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  let theyPlay = [];
-  for (let i = 0; i< musicians.length; i++){
-    theyPlay.push(musicians[i]+' plays '+instruments[i])
+  let theyPlay = []
+  for(let i = 0; i<musicians.length; i++){
+    theyPlay.push(musicians[i]+' plays '+ instruments[i])
   }
   return theyPlay
 }
 
 function johnLennonFacts(facts){
-  let newFacts = [];
-  for(let i = 0; i< facts.length; i++){
-    newFacts.push(facts[i]+'!!!')
+  let newFact = [];
+  let i=0;
+  while(i<facts.length){
+    newFact.push(facts[i] + '!!!')
+    i++;
   }
-  return newFacts
+  return newFact
 }
 
 function iLoveTheBeatles(n){
-  let love = [];
+  let love = []
   do{
     love.push('I love the Beatles!')
-    n++;
-  }
-  while (n<15)
+    n++
+  }while(n<15)
 return love
 }
